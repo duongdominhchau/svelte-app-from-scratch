afd16480ec6ff7535dfd44b2211c593d60f1bdf0
Chau Duong <duongdominhchau@gmail.com>
2024-09-07 13:37:12 +0700

    Use a plugin to auto-generate index.html
    After this step, `index.html` is in `dist` as well, so we only need to
    deploy everything inside `dist` directory

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,14 @@
+import html from "@rollup/plugin-html";
+
 export default {
     input: "src/index.js",
     output: {
         file: "bundle.js",
         format: "cjs",
     },
+    plugins: [
+        html({
+            title: "Svelte app from scratch",
+        }),
+    ],
 };
